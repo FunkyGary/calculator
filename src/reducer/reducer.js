19ab0172b3ee97e3048043ef1840ca2d65c8952d
Gary Huang <gary80212@gmail.com>
2020-11-07 23:09:04 +0800

    caculate done

---
+++ src/reducer/reducer.js
@@ -0,0 +1,65 @@
+const initState = {
+  result: 0,
+  firstNumber: "",
+  SecondNumber: "",
+  operator: null,
+};
+
+export const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case "OPERATOR": {
+      if (action.value === "=") {
+        console.log(
+          `${state.firstNumber} ${state.operator} ${state.SecondNumber}`
+        );
+        state.result = eval(
+          `${state.firstNumber} ${state.operator} ${state.SecondNumber}`
+        );
+        state.firstNumber = state.result;
+        state.SecondNumber = "";
+        state.operator = "=";
+      } else if (action.value === "AC") {
+        state = {
+          result: 0,
+          firstNumber: "",
+          SecondNumber: "",
+          operator: null,
+        };
+        console.log(state);
+      } else {
+        state.operator = action.value;
+      }
+      console.log(state);
+      return state;
+    }
+    case "NUMBER": {
+      if (
+        state.result > 0 &&
+        state.SecondNumber === "" &&
+        state.operator === "="
+      ) {
+        state.firstNumber = "";
+        state.firstNumber = state.firstNumber += action.value;
+        state.result = state.firstNumber;
+        state.operator = null;
+      } else {
+        if (state.operator === null) {
+          if (action.value === "0" && state.result === 0) {
+            return state;
+          } else {
+            state.firstNumber = state.firstNumber += action.value;
+            state.result = state.firstNumber;
+          }
+        } else {
+          state.SecondNumber = state.SecondNumber += action.value;
+          state.result = state.SecondNumber;
+        }
+      }
+      console.log(state);
+      return state;
+    }
+    default: {
+      return state;
+    }
+  }
+};
